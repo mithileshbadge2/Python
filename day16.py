ffe9533f438382dd223f6c1897bc920740900a9a
mithileshbadge2 <mithileshbadge2@gmail.com>
2024-05-22 20:33:35 +0530

    added

---
+++ day16.py
@@ -0,0 +1,52 @@
+# data = ['mithilesh','badge',24,38]
+# print(data[0])
+# data[0] = 'monty'
+# print(data)
+# data.append('nagpur')
+# print(data)
+# data.pop()
+# print(data)
+# data.pop(2)
+# print(data)
+# print('Monty' in data)
+
+# data = {
+#     "firstName" : 'monty',
+#     "lastName" : 'badge',
+#     "age" : 24,
+#     "rollNo" : 38
+# }
+# print(data)
+# a = data["firstName"]
+# print(a)
+# b = data['rollNo'] = 39
+# print(b)
+# c = data['city'] = 'nagpur'
+# print(c)
+# d = data.pop("rollNo")
+# print(d)
+# print(data)
+
+# del data
+# print(data)
+
+vehicle = {
+    'color':'red',
+    'type':'sedan'
+}
+print(vehicle)
+vehicle['color'] = 'green'
+print(vehicle)
+vehicle['regNo'] = 8989
+print(vehicle)
+vehicle.pop('regNo')
+print(vehicle)
+
+print('type' in vehicle)
+
+print(len(vehicle))
+# del vehicle
+# print(vehicle)
+
+
+
